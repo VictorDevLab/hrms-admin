29651b61d6ae0f860ff80d9b8fbe42c4269b447a
Victor Koome <koomev786@gmail.com>
2025-05-26 09:01:09 +0300

    configure router and vuetify

---
+++ src/pages/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1>About Us</h1>
+    <p>This is the about page where you can learn more about our application.</p>
+    <p>Vue Router makes navigation between pages seamless!</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.about {
+  text-align: center;
+  padding: 2rem;
+}
+
+h1 {
+  color: #35495e;
+}
+</style>
