315e2edf24fe7f602cad936419e3e93b33c579ef
Victor Koome <koomev786@gmail.com>
2025-06-23 10:42:42 +0300

    add production domain

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://hrms-api-arw0.onrender.com',
   withCredentials: true, 
 });
 
