55d61b64142d48388a3c6ffd958d3316a58f24ca
Victor Koome <koome.v@nathandigital.com>
2025-12-02 12:18:48 +0300

    code clean up

--- src/pages/payroll/index.vue
+++ src/pages/payroll/index.vue
@@ -6,14 +6,8 @@
       <v-icon size="64" color="primary" class="mb-4">mdi-clock-outline</v-icon>
       <h2 class="text-h5 font-weight-bold mb-2">Payroll Module</h2>
       <p class="text-body-1 mb-6">
-        We're working hard to bring you this feature soon. Stay tuned!
+        Module under Development
       </p>
-      <v-progress-linear
-        color="primary"
-        indeterminate
-        height="6"
-        rounded
-      ></v-progress-linear>
     </v-card>
   </v-container>
 </template>
